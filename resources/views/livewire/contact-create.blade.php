45411c938cdcedd44dac4f839541e0f3e3e09b37
fathurwalkers <fathurwalkers44@gmail.com>
2020-08-23 17:51:32 +0800

    menambahkan validasi

--- resources/views/livewire/contact-create.blade.php
+++ resources/views/livewire/contact-create.blade.php
@@ -3,11 +3,22 @@
         <div class="form-group">
             <div class="form-row">
                 <div class="col">
-                    <input wire:model="name" type="text" name="" id="" class="form-control" placeholder="Name..">
+                    <input wire:model="name" type="text" name="" id=""
+                        class="form-control @error('name') is-invalid @enderror" placeholder="Name..">
+                    @error('name')
+                    <span class="invalid-feedback">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
                 <div class="col">
-                    <input wire:model="phone" type="text" name="" id="" class="form-control"
-                        placeholder="Phone number..">
+                    <input wire:model="phone" type="text" name="" id=""
+                        class="form-control @error('phone') is-invalid @enderror" placeholder="Phone number..">
+                    @error('phone')
+                    <span class="invalid-feedback">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
                 </div>
             </div>
         </div>
